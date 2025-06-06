a69efc1e69f2f8d2f2db175a3dede5846b9d7fde
_.mindang <minhdang875425@gmail.com>
2025-06-06 12:43:34 +0700

    push

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Stack, Box } from '@mui/material'
+import { VideoCard, ChannelCard } from './'
 
-export default function Videos({ videos }) {
-    console.log(videos)
+export default function Videos({ videos, direction }) {
+    if (!videos?.length)
+        return 'Loading...'
+
     return (
-        <div>Videos</div>
+        <Stack direction={ direction || 'row' } flexWrap='wrap'  justifyContent='start' gap={2}>
+            {videos.map((item, index) => (
+                <Box key={index}>
+                    {item.id.videoId && <VideoCard video={item} />}
+                    {item.id.channelId && <ChannelCard channelDetail={item} />}
+                </Box>
+            ))}
+        </Stack>
     )
 }
